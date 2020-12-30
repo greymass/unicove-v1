0c156765a4477bb1fba272ef19605696afe78b1d
dafuga <danielfugere28@gmail.com>
2020-12-30 12:57:53 -0500

    chore: changed the menu urls

--- src/components/navigation/sidebar.svelte
+++ src/components/navigation/sidebar.svelte
@@ -4,8 +4,8 @@
 
 <aside class="absolute w-half h-full bg-gray-200 border-r-2 shadow-lg" class:open>
 	<nav class="p-12 text-xl">
-		<a class="block" href="#dashboard">Dashboard</a>
-		<a class="block" href="#transfer">Transfer</a>
+		<a class="block" href="/">Dashboard</a>
+		<a class="block" href="/transfer">Transfer</a>
 	</nav>
 </aside>
 
