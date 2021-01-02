64878e247b76cbb71f9d92a0461fcdbb4670453b
dafuga <danielfugere28@gmail.com>
2021-01-02 18:51:04 -0500

    chore: made the expand button a button

--- src/components/layout/navigation/navbar.svelte
+++ src/components/layout/navigation/navbar.svelte
@@ -1,17 +1,20 @@
 <script>
-	import Hamburger from './hamburger.svelte'
-	import Menu from './menu.svelte'
+	import Hamburger from './navbar/hamburger.svelte'
 
 	import expandIcon from '../../../../public/images/expand.svg'
 
 	export let sidebar = false
+
+	let expand = false;
 </script>
 
 <div class="header h-full w-1/2 p-2 items-center text-gray-600 border-r-2">
     <div class="hamburger-container">
         <Hamburger bind:open={sidebar}/>
     </div>
-    <img class="expand-icon" src={expandIcon} />
+    <a class="expand-icon" on:click={() => expand = !expand}>
+        <img src={expandIcon} />
+    </a>
 </div>
 
 <style>
@@ -26,7 +29,11 @@
     width: 25px;
   }
 
-  .header img.expand-icon {
+  .header a.expand-icon {
+    cursor: pointer;
+  }
+
+  .header a.expand-icon img {
     width: 40px;
     margin: auto;
     margin-top: 40px;
