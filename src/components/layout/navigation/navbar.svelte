b242410e6a7a0877a2373d2fc556fb7406d11e28
dafuga <danielfugere28@gmail.com>
2021-01-03 12:11:03 -0500

    refactor: refactored navbar files

--- src/components/layout/navigation/navbar.svelte
+++ src/components/layout/navigation/navbar.svelte
@@ -1,61 +1,18 @@
 <script>
-	import Hamburger from './navbar/hamburger.svelte'
 
-	import expandIcon from '../../../../public/images/expand.svg'
-	import arrowRight from '../../../../public/images/arrow-right.svg'
+	import LargeNavbar from './navbar/large.svelte';
+	import SmallNavbar from './navbar/small.svelte'
 
-	export let sidebar = false
+	export let sidebar = false;
 
 	let expand = false;
 </script>
 
-<div class="header h-full w-1/2 p-2 items-center text-gray-600 border-r-2">
-    <div class="hamburger-container">
-        <Hamburger bind:open={sidebar}/>
-    </div>
-    <hr />
-    <div class="expand-icon" on:click={() => expand = !expand}>
-        <img src={expandIcon} />
-    </div>
-    <a class="arrow-button" on:click={() => expand = !expand}>
-        <div class="arrow-icon-container">
-             <img src={arrowRight} />
-        </div>
-    </a>
-</div>
+{#if expand}
+  <LargeNavbar />
+{:else}
+  <SmallNavbar sidebar={sidebar} onExpand={() => expand = true}/>
+{/if}
 
 <style>
-  .header {
-    background-color: var(--main-grey);
-    padding: 25px;
-    padding-top: 30px;
-  }
-
-  .header .hamburger-container {
-    margin: auto;
-    margin-bottom: 30px;
-    width: 25px;
-  }
-
-  .header .expand-icon img {
-    width: 40px;
-    margin: auto;
-    margin-top: 40px;
-  }
-
-  .header a.arrow-button .arrow-icon-container {
-    cursor: pointer;
-    background-color: white;
-    width: 60px;
-    height: 50px;
-    margin: auto;
-    margin-top: 40px;
-    padding: 15px;
-    padding-top: 10px;
-    border-radius: 3px;
-  }
-
-  .header a.arrow-button img {
-    width: 30px;
-  }
 </style>
---
+++ src/components/layout/navigation/navbar/small.svelte
@@ -0,0 +1,60 @@
+<script>
+	import Hamburger from './small/hamburger.svelte'
+
+	import expandIcon from '../../../../../public/images/expand.svg'
+	import arrowRight from '../../../../../public/images/arrow-right.svg'
+
+	export let sidebar = false;
+	export let onExpand;
+</script>
+
+<div class="header h-full w-1/2 p-2 items-center text-gray-600 border-r-2">
+    <div class="hamburger-container">
+        <Hamburger bind:open={sidebar}/>
+    </div>
+    <hr />
+    <div class="expand-icon">
+        <img src={expandIcon} />
+    </div>
+    <a class="arrow-button" on:click={onExpand}>
+        <div class="arrow-icon-container">
+             <img src={arrowRight} />
+        </div>
+    </a>
+</div>
+
+<style>
+  .header {
+    background-color: var(--main-grey);
+    padding: 25px;
+    padding-top: 30px;
+  }
+
+  .header .hamburger-container {
+    margin: auto;
+    margin-bottom: 30px;
+    width: 25px;
+  }
+
+  .header .expand-icon img {
+    width: 40px;
+    margin: auto;
+    margin-top: 40px;
+  }
+
+  .header a.arrow-button .arrow-icon-container {
+    cursor: pointer;
+    background-color: white;
+    width: 60px;
+    height: 50px;
+    margin: auto;
+    margin-top: 40px;
+    padding: 15px;
+    padding-top: 10px;
+    border-radius: 3px;
+  }
+
+  .header a.arrow-button img {
+    width: 30px;
+  }
+</style>
