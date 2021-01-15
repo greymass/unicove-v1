b61ec107ae443cf4eb89c83e3f9e7e9ffbeedffb
Aaron Cox <aaron@greymass.com>
2021-01-15 00:23:42 -0500

    Created account input component

---
+++ src/components/elements/input/account.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import {Name} from '@greymass/eosio'
+
+    import Input from '~/components/elements/input.svelte'
+
+    export let name: string = ''
+    export let value: string = ''
+
+    export let onChange: any = () => {}
+
+    const validate = (value: string) => {
+        try {
+            return Name.from(value).toString() === value
+        } catch (error) {
+            // might be nice to catch errors for form validation errors
+            return false
+        }
+    }
+</script>
+
+<style type="scss">
+</style>
+
+<Input {onChange} {name} bind:value isValid={validate} />
