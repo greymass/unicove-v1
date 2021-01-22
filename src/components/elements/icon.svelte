48d3fa439bc4b792846f61993f24615d09a8cdb5
Aaron Cox <aaron.cox@greymass.com>
2021-01-22 13:28:07 -0500

    Icons, Progress Bar, and Fuel (#29)
    * Added generic progress bar element
    
    * Initial mock data + UI
    
    * remove debug
    
    * Defining types for API responses
    
    * Added icons
    
    * Buttons with Icons
    
    * Adding type definitions for feather icons
    
    * Removing images
    
    * Remove old svg
    
    * Rework navigation to use icons

---
+++ src/components/elements/icon.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import {icons} from 'feather-icons'
+
+    export let name = 'help-circle'
+    export let dimensions = '1em'
+    export let size = 'default'
+
+    if (!icons[name]) {
+        name = 'help-circle'
+    }
+
+    switch (size) {
+        case 'tiny': {
+            dimensions = '0.5em'
+            break
+        }
+        case 'small': {
+            dimensions = '0.75em'
+            break
+        }
+        case 'default': {
+            dimensions = '1em'
+            break
+        }
+        case 'medium': {
+            dimensions = '1.25em'
+            break
+        }
+        case 'large': {
+            dimensions = '1.5em'
+            break
+        }
+        case 'huge': {
+            dimensions = '2.5em'
+            break
+        }
+        case 'massive': {
+            dimensions = '4em'
+            break
+        }
+    }
+</script>
+
+<style type="scss">
+</style>
+
+{@html icons[name].toSvg({
+    height: dimensions,
+    width: dimensions,
+})}
--- src/pages/_components/buttons.svelte
+++ src/pages/_components/buttons.svelte
@@ -1,7 +1,5 @@
 <script>
     import Button from '~/components/elements/button.svelte'
-    import InfoIcon from '@/images/user-blue.svg'
-    console.log(InfoIcon)
 </script>
 
 <style lang="scss">
@@ -22,45 +20,20 @@
 <div>
     <Button>Standard</Button>
     <Button primary>Primary</Button>
+    <Button icon="external-link">Icon</Button>
+    <Button icon="external-link" iconPosition="left">Icon left</Button>
 </div>
 
 <div>
     <Button size="large">Large</Button>
     <Button size="large" primary>Large Primary</Button>
+    <Button size="large" icon="external-link">Large with icon</Button>
+    <Button size="large" icon="external-link" iconPosition="left">Large with icon left</Button>
 </div>
 
 <div class="overunder">
     <Button size="large">Flexy</Button>
-    <Button size="large" primary>
-        <!-- TODO: icon component -->
-        <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M12.6667 2H3.33333C2.59695 2 2 2.59695 2 3.33333V12.6667C2 13.403 2.59695 14 3.33333 14H12.6667C13.403 14 14 13.403 14 12.6667V3.33333C14 2.59695 13.403 2 12.6667 2Z"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            />
-            <path
-                d="M2 6H14"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            />
-            <path
-                d="M6 14V6"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            />
-        </svg>
-        &nbsp; Flexy with icon
-    </Button>
+    <Button size="large" primary>Flexy Primary</Button>
+    <Button size="large" primary icon="layout">Flexy with icon</Button>
+    <Button size="large" primary icon="layout" iconPosition="left">Flexy with icon left</Button>
 </div>
