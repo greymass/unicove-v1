8582b905961bc7e5705ee30d18452ed6dd42aad1
dafuga <danielfugere28@gmail.com>
2021-02-01 18:22:23 -0800

    refactor: added interface util file

--- src/pages/transfer/form.svelte
+++ src/pages/transfer/form.svelte
@@ -4,13 +4,19 @@
     import InputAccount from '~/components/elements/input/account.svelte'
     import InputAsset from '~/components/elements/input/asset.svelte'
     import Form from '~/components/elements/form.svelte'
+
+    export let toAddress;
+    export let toAccount;
+    export let activeBlockchain;
+    export let amount;
+    export let memo;
 </script>
 
 <Form>
     <label>
       To
     </label>
-    {#if $activeBlockchain.id === 'fio'}
+    {#if activeBlockchain.id === 'fio'}
         <Input name="to" bind:value={toAddress} />
     {:else}
         <InputAccount name="to" bind:value={toAccount} />
@@ -18,7 +24,7 @@
     <label>
         Amount
     </label>
-    <InputAsset name="amount" bind:value />
+    <InputAsset name="amount" bind:value={amount} />
     {#if $activeBlockchain.id === 'fio'}
     <label>
          Memo <i>(Optional)</i>
---
+++ src/utils/interfaces.ts
@@ -0,0 +1,17 @@
+export interface Transfer {
+    quantity: String;
+    toAccount: String;
+    memo: String;
+    auth: Object;
+    transact: Function;
+    coreTokenContract: Object;
+    coreTokenTransfer: Object;
+}
+
+export interface Blockchain {
+
+}
+
+export interface Session {
+
+}
