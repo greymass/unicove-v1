b30e6497fdfb817913c72e249fda99add6420721
dafuga <danielfugere28@gmail.com>
2021-02-03 18:39:30 -0800

    chore: added proper data types for all transfer methods

--- src/services/eosio/transfer/default.ts
+++ src/services/eosio/transfer/default.ts
@@ -1,30 +1,41 @@
-import {FIOTransfer, Transfer} from "~/abi-types";
+import type {LinkSession} from 'anchor-link';
+import type {Transfer} from '~/abi-types';
+import type {ChainConfig} from '~/config';
+
+interface TransferData {
+
+}
 
 export async function defaultTransfer(
-    activeSession: Object,
-    activeBlockchain: Object,
-    properties: Object
+    activeSession: LinkSession,
+    activeBlockchain: ChainConfig,
+    transferProperties: Transfer,
 ) {
-    const data = generateTransfer(activeSession, properties);
+    const data = generateTransfer(activeSession, transferProperties);
+
+    console.log({data})
 
     transact(activeBlockchain, activeSession, data);
 }
 
-function generateTransfer(activeSession: Object, properties: Object) {
-    const { quantity, toAccount, memo } = properties;
+function generateTransfer(
+    activeSession: LinkSession,
+    transferProperties: Transfer,
+) {
+    const { quantity, to, memo } = transferProperties;
 
     return Transfer.from({
         from: activeSession!.auth.actor,
-        to: toAccount,
+        to,
         quantity,
         memo,
     });
 }
 
 async function transact(
-    activeSession: Object,
-    activeBlockchain: Object,
-    data: Object
+    activeSession: LinkSession,
+    activeBlockchain: ChainConfig,
+    data: TransferData
 ) {
     return await activeSession!.transact({
         action: {
