254bff25f7c4346267426e6e2d3670ce4a2f4a6e
dafuga <danielfugere28@gmail.com>
2021-02-09 21:51:07 -0800

    chore: added validation messages to account field

--- src/components/elements/input/account.svelte
+++ src/components/elements/input/account.svelte
@@ -5,19 +5,63 @@
 
     export let name: string = ''
     export let value: string = ''
+    export let errorMessage: string | null
 
     const validate = (value: string) => {
         try {
-            if (value.length === 0) return false
-            return Name.from(value).toString() === value
-        } catch (error) {
-            // might be nice to catch errors for form validation errors
-            return false
+            validatePresence(value)
+            validateLength(value)
+            validateIsString(value)
+        } catch (errorObject) {
+            errorMessage =  errorObject.message
+
+            return false;
         }
+
+        return true
+    }
+
+    function validatePresence(value: string) {
+        if (value.length === 0) {
+              throw {
+                valid: false,
+                message: null,
+              }
+        }
+    }
+
+    function validateLength(value: string) {
+       console.log('validateLength check')
+       if (value.length > 13) {
+         console.log('validateLength hit')
+         console.log({value})
+          throw {
+            valid: false,
+            message: 'An account name should be 13 characters or less.'
+          }
+       }
+    }
+
+    function validateIsString(value: string) {
+       if (Name.from(value).toString() !== value) {
+         console.log('in validateIsString')
+         console.log(Name.from(value).toString())
+         console.log({value})
+         throw {
+           valid: false,
+           message: 'Is not a valid account name.'
+         };
+       }
+    }
+
+    $: {
+      console.log('input rendering')
+      console.log({errorMessage})
     }
 </script>
 
 <style type="scss">
 </style>
 
-<Input on:changed {name} bind:value isValid={validate} />
+<Input on:changed {name} bind:value isValid={validate} errorMessage="{errorMessage}" />
+
