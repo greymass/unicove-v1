cdc9fa246c1c15c423bedeaec55b182b079cdb8b
dafuga <danielfugere28@gmail.com>
2021-02-11 21:24:34 -0800

    enhancement: closing modal when clicking on greyed out area

--- src/components/elements/modal.svelte
+++ src/components/elements/modal.svelte
@@ -5,9 +5,14 @@
     export let opened: boolean = false
     export let header: string | null
     export let size: string | null
+    export let hideCloseButton: boolean = false
 
     const handleClose = function() {
       opened = false
+    }
+
+    const preventClose = function(event) {
+      event.stopPropagation();
     }
 </script>
 
@@ -20,12 +25,12 @@
       height: 100%; /* Full height */
       overflow: auto; /* Enable scroll if needed */
       background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+      z-index: 999;
 
       .modal {
         background-color: white;
         position: relative;
         margin: 15% auto; /* 15% from the top and centered */
-        padding: 20px;
         border: 1px solid #888;
         width: 80%; /* Could be more or less, depending on screen size */
         max-width: 300px;
@@ -36,40 +41,47 @@
          }
 
          .modal-header {
-           padding: 10px;
+           padding: 20px 40px;
+         }
+
+         hr {
+           margin: 0;
          }
 
           .modal-content {
-            padding: 20px 10px;
+            padding: 20px;
           }
 
          .button-container {
             display: flex;
             flex-direction: column;
+            padding: 20px;
          }
       }
   }
 </style>
 
 {#if opened}
-<div class="container">
-  <div class={`modal ${size}`}>
+<div on:click={handleClose} class="container">
+  <div on:click={preventClose} class={`modal ${size}`}>
     {#if header}
         <div class="modal-header">
-            <h2>
+            <h1>
                 {header}
-            </h2>
+            </h1>
         </div>
         <hr />
     {/if}
     <div class="modal-content">
         <slot />
     </div>
-    <div class="button-container">
-        <Button size="large" on:action={handleClose}>
-           Close
-        </Button>
-    </div>
+    {#if !hideCloseButton}
+        <div class="button-container">
+            <Button size="large" on:action={handleClose}>
+               Close
+            </Button>
+        </div>
+    {/if}
   </div>
 </div>
 {/if}
