b56a1944c8e675078e9b7d56d7b8e34e20ac4926
Aaron Cox <aaron@greymass.com>
2021-02-12 16:25:12 -0500

    Few tweaks to the styles to match figma

--- src/components/elements/modal.svelte
+++ src/components/elements/modal.svelte
@@ -27,24 +27,35 @@
         z-index: 999;
 
         .modal {
+            font-family: Inter;
+            font-style: normal;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 19px;
+            text-align: center;
+            letter-spacing: -0.26px;
             background-color: white;
             position: relative;
             margin: 15% auto;
-            border: 1px solid #888;
+
+            box-shadow: 0px 24px 32px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
+                0px 4px 8px rgba(0, 0, 0, 0.04), 0px 0px 1px rgba(0, 0, 0, 0.04);
+            border-radius: 20px;
+
             width: 80%;
             max-width: 300px;
-            border-radius: 20px;
 
             &.large {
                 max-width: 500px;
             }
 
             .modal-header {
-                padding: 20px;
-            }
-
-            hr {
-                margin: 0;
+                color: #585d6e;
+                font-weight: bold;
+                font-size: 18px;
+                line-height: 22px;
+                letter-spacing: -0.26px;
+                padding: 21px;
             }
 
             .modal-content {
@@ -54,7 +65,7 @@
             .button-container {
                 display: flex;
                 flex-direction: column;
-                padding: 20px;
+                padding: 17px;
             }
         }
     }
@@ -69,7 +80,6 @@
                         {header}
                     </h1>
                 </div>
-                <hr />
             {/if}
             <div class="modal-content">
                 <slot />
