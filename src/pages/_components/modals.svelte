140194b35a7cf41719c83813a679f145135ead7c
Aaron Cox <aaron@greymass.com>
2021-02-12 19:04:45 -0500

    Updated examples

--- src/pages/_components/modals.svelte
+++ src/pages/_components/modals.svelte
@@ -2,9 +2,15 @@
     import Modal from '~/components/elements/modal.svelte'
     import Button from '~/components/elements/button.svelte'
 
+    let callbackModalDisplayed = false
     const callbackAction = () => {
-        callbackModalOpened = false
         alert('Callback Triggered!')
+        callbackModalDisplayed = false
+    }
+
+    let displayExternallyControlled = false
+    const showExternallyControlled = () => {
+        displayExternallyControlled = true
     }
 </script>
 
@@ -15,32 +21,42 @@
 </style>
 
 <div>
-    <Modal header="Test Header">
-        <span slot="trigger">
-            <Button>Open Small Modal</Button>
-        </span>
-        <p>This is where your modal body goes.</p>
-    </Modal>
+    <div>
+        <Modal header="Test Header">
+            <span slot="trigger">
+                <Button>Open Small Modal</Button>
+            </span>
+            <p>This is where your modal body goes.</p>
+        </Modal>
+        <Modal size="large">
+            <span slot="trigger">
+                <Button>Open Large Modal without header</Button>
+            </span>
+            <p>This is where your modal body goes.</p>
+        </Modal>
 
-    <Modal size="large">
-        <span slot="trigger">
-            <Button>Open Large Modal without header</Button>
-        </span>
-        <p>This is where your modal body goes.</p>
-    </Modal>
+        <Modal hideCloseButton>
+            <span slot="trigger">
+                <Button>Open Modal without close button</Button>
+            </span>
+            <p>This is where your modal body goes.</p>
+        </Modal>
 
-    <Modal hideCloseButton>
-        <span slot="trigger">
-            <Button>Open Modal without close button</Button>
-        </span>
-        <p>This is where your modal body goes.</p>
-    </Modal>
+        <Modal hideCloseButton bind:display={callbackModalDisplayed}>
+            <span slot="trigger">
+                <Button>Open Modal with custom action</Button>
+            </span>
+            <p>This is where your modal body goes.</p>
+            <Button on:action={callbackAction}>Click me!</Button>
+        </Modal>
+    </div>
 
-    <Modal hideCloseButton>
-        <span slot="trigger">
-            <Button>Open Modal with custom action</Button>
-        </span>
-        <p>This is where your modal body goes.</p>
-        <Button on:action={callbackAction}>Click me!</Button>
-    </Modal>
+    <div>
+        <Button on:action={showExternallyControlled}>Open Externally Controlled Modal</Button>
+        Opened: {displayExternallyControlled}
+
+        <Modal header="Test Header" bind:display={displayExternallyControlled}>
+            <p>This is where your modal body goes.</p>
+        </Modal>
+    </div>
 </div>
