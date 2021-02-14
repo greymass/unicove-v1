17b8504ab94e0deda24cf8338307561696ef219a
Aaron Cox <aaron@greymass.com>
2021-02-14 14:47:26 -0500

    Segment basics

---
+++ src/components/elements/segment/group.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let vertical: boolean = false
+</script>
+
+<style type="scss">
+    .segment-group {
+        display: flex;
+        flex-direction: row;
+        gap: 12px;
+        margin: 0 0 20px;
+        :global(.segment) {
+            flex-grow: 1;
+        }
+        &.vertical {
+            flex-direction: column;
+        }
+    }
+</style>
+
+<div class="segment-group" class:vertical>
+    <slot />
+</div>
