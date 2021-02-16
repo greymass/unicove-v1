c82563e566951471334f56b4d97093650251fa89
Aaron Cox <aaron@greymass.com>
2021-02-16 16:22:39 -0500

    Fixed selector.

--- src/components/elements/segment/group.svelte
+++ src/components/elements/segment/group.svelte
@@ -9,7 +9,7 @@
         flex-wrap: wrap;
         margin: calc(-1 * var(--gap)) 0 0 calc(-1 * var(--gap));
         width: calc(100% + var(--gap));
-        :global(> *) {
+        > :global(*) {
             margin: var(--gap) 0 0 var(--gap);
         }
         :global(.segment) {
