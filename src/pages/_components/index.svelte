b7b3402c5c2481449ea50e8c242e304ed35eb29b
Aaron Cox <aaron.cox@greymass.com>
2021-02-16 16:25:07 -0500

    Segments (#40)
    * Segment basics
    
    * Changing to margins since gap isn't well supported.
    
    * Fixed selector.

--- src/pages/_components/index.svelte
+++ src/pages/_components/index.svelte
@@ -6,6 +6,7 @@
     import Icons from './icons.svelte'
     import Inputs from './inputs.svelte'
     import Progress from './progress.svelte'
+    import Segments from './segments.svelte'
 
     import Nav from '~/components/elements/nav.svelte'
 
@@ -15,6 +16,7 @@
         {name: 'Icons', path: 'icons', component: Icons},
         {name: 'Inputs', path: 'inputs', component: Inputs},
         {name: 'Progress Bar', path: 'progress', component: Progress},
+        {name: 'Segments', path: 'segment', component: Segments},
     ]
 </script>
 
