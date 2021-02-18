789010633d5793e5ae779acca3b9a7d6d7ae98f2
dafuga <danielfugere28@gmail.com>
2021-02-17 16:27:46 -0800

    chore: passing placeholder to input fields

--- src/components/elements/input/address.svelte
+++ src/components/elements/input/address.svelte
@@ -8,6 +8,7 @@
     export let valid: boolean = false
     export let focus: boolean = false
     export let fullWidth: boolean = false
+    export let placeholder: string | undefined = undefined
 
     const validate = async (value: string) => {
         try {
@@ -37,6 +38,6 @@
 </style>
 
 <div>
-    <Input on:changed {name} {fullWidth} {focus} bind:value isValid={validate} />
+    <Input on:changed {name} {fullWidth} {focus} {placeholder} bind:value isValid={validate} />
     <ErrorMessage {errorMessage} />
 </div>
