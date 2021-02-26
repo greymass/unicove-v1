1ed7de07b3011a3f3db48cbd12abeccdd18b0e5e
Daniel Fugere <danielfugere28@gmail.com>
2021-02-27 08:59:21 +0900

    Modal component (#37)
    * chore: added modal component
    
    * chore: added modal components to _component page
    
    * enhancement: closing modal when clicking on greyed out area
    
    * cleanup: code cleanup
    
    * format: prettified
    
    * cleanup: removed some comments
    
    * chore: fixed typo
    
    * fix: little styling adjustment on modal header
    
    * Few tweaks to the styles to match figma
    
    * Switched to a named slot
    
    * Added example with custom callback button
    
    * Moved open logic to internal state and split removed container to add dimmer
    
    * Updated examples
    
    * Allow disabling of dimmer click to close
    
    * Prevent empty html element from appending when slot is not defined.
    
    * Removed named slot and instead using parent trigger
    
    Co-authored-by: Aaron Cox <aaron@greymass.com>
    Co-authored-by: Aaron Cox <aaron.cox@greymass.com>

---
+++ src/components/elements/modal/trigger.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Button from '~/components/elements/button.svelte'
+    import {writable} from 'svelte/store'
+
+    export let content = 'Open Modal'
+    export let disabled: boolean = false
+    export let display = writable<boolean>(false)
+
+    const open = () => {
+        if (!disabled) {
+            $display = true
+        }
+    }
+
+    let close = () => ($display = false)
+</script>
+
+<style type="scss">
+</style>
+
+<Button {disabled} on:action={open}>
+    {content}
+</Button>
+
+<slot {display} {close} />
