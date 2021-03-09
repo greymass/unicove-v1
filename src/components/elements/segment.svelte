ea3d6bd69c970a58f9ceed9637728e8f2116f1ef
Aaron Cox <aaron@greymass.com>
2021-03-09 01:28:13 -0500

    Adding colors for segments

--- src/components/elements/segment.svelte
+++ src/components/elements/segment.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    export let color = ''
 </script>
 
 <style type="scss">
@@ -6,9 +7,12 @@
         background: #f7f7fc;
         border-radius: 20px;
         padding: 16px 26px;
+        &.white {
+            background: var(--main-white);
+        }
     }
 </style>
 
-<div class="segment">
+<div class="segment {color}">
     <slot />
 </div>
