abdee6add54ee4bc89d8d3748c4a20a8a10a3262
dafuga <danielfugere28@gmail.com>
2021-03-09 18:51:58 -0800

    format: code formatting

--- src/pages/tokens/tokensData.ts
+++ src/pages/tokens/tokensData.ts
@@ -1,17 +1,15 @@
-import {writable} from 'svelte/store'
+import {writable, get} from 'svelte/store'
+
+import type {LinkSession} from 'anchor-link'
 
 import type {TokensData} from './types'
 
-export const tokensData = writable<{ string: TokensData }>({step: Step.Recipient})
+export const tokensData = writable<{ string: TokensData } | undefined>(undefined)
 
-import {writable, get} from 'svelte/store'
-import {Asset, UInt64, LinkSession} from 'anchor-link'
 import type {ChainConfig} from '~/config'
 import {activeBlockchain, activeSession} from '~/store'
 
 let interval: any
-
-export const tokensData = writable<Asset | undefined>(undefined)
 
 export function syncTokenBalances() {
     interval = setInterval(() => {
@@ -29,19 +27,26 @@ export async function fetchTxFee() {
     const session: LinkSession | undefined = get(activeSession)
     const blockchain: ChainConfig = get(activeBlockchain)
 
+    if (!session) {
+        return;
+    }
+
     const apiUrl = `https://www.api.bloks.io/${
         blockchain.id
     }/account/${
-        session.accountName
+        session.auth.actor
     }?type=getAccountTokens&coreSymbol=${
-        blockchain.symbol
+        blockchain.coreTokenSymbol
     }`
 
     const apiResponse = await fetch(apiUrl)
 
+    console.log({apiResponse})
+
     tokensData.set(parseResponse(apiResponse))
 }
 
-function parseResponse(apiResponse) {
+function parseResponse(apiResponse: any) : { string: TokensData } {
     // Parse response here
+    return {}
 }
