6aea0f53347099b22a9b181dfd88400ae0426e07
dafuga <danielfugere28@gmail.com>
2021-03-11 18:47:31 -0800

    chore: fetching tokenBalances on transfer page

--- src/pages/transfer/transferData.ts
+++ src/pages/transfer/transferData.ts
@@ -5,13 +5,19 @@ import type {TransferData} from './types'
 import {Step} from './types'
 
 import type {ChainConfig} from '~/config'
-import {activeBlockchain} from '~/store'
+import {activeBlockchain, activeSession} from '~/store'
+import {tokenBalancesTicker} from '~/token-balances-ticker'
 
 export const transferData = writable<TransferData>({step: Step.Recipient})
 
 export const quantity = derived<typeof transferData, Asset>(transferData, (data, set) => {
     const blockchain: ChainConfig = get(activeBlockchain)
+    const session: ChainConfig = get(activeSession)
+
     let parsed: number = parseFloat(data.amount || '')
+    const tokenBalances = tokenBalancesTicker(session, $activeBlockchain).catch((error) => {
+        console.warn(`Unable to load price on ${$activeBlockchain.id}`, error)
+    })
     const tokenBalancesObject = data.token && get(tokenBalances) // add that to store.ts
     const token = tokenBalancesObject && tokenBalancesObject[data.token]
     const assetSymbol = token ? blockchain.coreTokenSymbol :
