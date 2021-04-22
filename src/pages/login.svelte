03320307df2741e1bf152817ae62ab89aa476aa6
Aaron Cox <aaron@greymass.com>
2021-04-22 16:39:19 -0400

    Moved login button logic to its own shared element

--- src/pages/login.svelte
+++ src/pages/login.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
-    import {login} from '~/auth'
-    import Button from '~/components/elements/button.svelte'
-
-    let working = false
-    let error: Error | undefined
-
-    function loginHandler() {
-        working = true
-        login()
-            .then(() => {
-                error = undefined
-            })
-            .catch((err) => {
-                error = err
-            })
-            .finally(() => {
-                working = false
-            })
-    }
+    import ButtonLogin from '~/components/elements/button/login.svelte'
 </script>
 
 <style lang="scss">
@@ -32,23 +14,13 @@
         header {
             text-align: center;
         }
-        .error {
-            color: red;
-        }
     }
 </style>
 
 <div class="login">
-    {#if !working}
-        <header>
-            <h1>Welcome to Greymass Wallet</h1>
-            <p>Please login to continue</p>
-        </header>
-        <Button size="large" primary on:action={loginHandler}>Login with Anchor</Button>
-        {#if error}
-            <p class="error">That didn't go well... {error.message}</p>
-        {/if}
-    {:else}
-        <p>Working really hard here...</p>
-    {/if}
+    <header>
+        <h1>Welcome to Greymass Wallet</h1>
+        <p>Please login to continue</p>
+    </header>
+    <ButtonLogin>Login with Anchor</ButtonLogin>
 </div>
