3efc1f3c97856ca915749973ca9bb1942169876a
dafuga <danielfugere28@gmail.com>
2021-05-29 16:48:08 -0700

    chore: added image component

---
+++ src/components/elements/image.svelte
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+    export let src: string = ''
+    export let alt: string = ''
+    export let fallbackSrc: string = ''
+    export let fallbackAlt: string = ''
+
+    let srcToUse : string = src;
+    let altToUse : string = alt;
+
+    function useImageFallback() {
+        srcToUse = fallbackSrc
+        altToUse = fallbackAlt
+    }
+</script>
+
+<img src={srcToUse} alt={alt} on:error={useImageFallback} />
