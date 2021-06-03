0b4bf9d04e92f6b164ff083be0e914069014fa88
Aaron Cox <aaron@greymass.com>
2021-06-03 17:50:34 -0400

    Remove helper function and use the Asset class

--- src/components/elements/input/validators/asset.ts
+++ src/components/elements/input/validators/asset.ts
@@ -1,13 +1,8 @@
 import {Asset} from '@greymass/eosio'
 
-function unitsFromValue(value: string, symbol: Asset.Symbol) {
-    return Math.floor(parseFloat(value) * Math.pow(10, symbol.precision))
-}
-
 export function validateBalance(value: string, balance: Asset) {
-    const units = unitsFromValue(value, balance.symbol)
-
-    if (units > Number(balance.units)) {
+    const {units} = Asset.from(Number(value), balance.symbol)
+    if (Number(units) > Number(balance.units)) {
         throw {
             valid: false,
             message: 'Insufficient funds available.',
@@ -16,8 +11,8 @@ export function validateBalance(value: string, balance: Asset) {
 }
 
 export function validateIsNumber(value: string, symbol: Asset.Symbol) {
-    const units = unitsFromValue(value, symbol)
-    const unitsAreNotNumber = isNaN(units)
+    const {units} = Asset.from(Number(value), symbol)
+    const unitsAreNotNumber = isNaN(Number(units))
 
     if (unitsAreNotNumber) {
         throw {
@@ -28,8 +23,8 @@ export function validateIsNumber(value: string, symbol: Asset.Symbol) {
 }
 
 export function validateNonZero(value: string, symbol: Asset.Symbol) {
-    const units = unitsFromValue(value, symbol)
-    const isLessThanZero = Asset.fromUnits(units, symbol).value <= 0
+    const asset = Asset.from(Number(value), symbol)
+    const isLessThanZero = asset.value <= 0
 
     if (isLessThanZero) {
         throw {
