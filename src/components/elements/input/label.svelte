d76326e09ef99f85dbcc26d96fe68c9d58abd8f2
Aaron Cox <aaron.cox@greymass.com>
2021-06-07 10:24:19 -0400

    Fixes (#87)
    * Add optional border to segments
    
    * Added Input Labels
    
    * Changed border color of transfers
    
    * Adding semver identifier
    
    * Added early access to page title

---
+++ src/components/elements/input/label.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<style type="scss">
+    .label {
+        color: var(--main-black);
+        font-weight: 600;
+        font-size: 10px;
+        line-height: 12px;
+        letter-spacing: 0.1px;
+        text-transform: uppercase;
+        margin-bottom: 12px;
+    }
+</style>
+
+<div class="label">
+    <slot />
+</div>
