eb05e33cceeaeb0265354147680d83015938ec9e
dafuga <danielfugere28@gmail.com>
2021-11-25 22:32:40 -0800

    chore: added account creation page

---
+++ src/pages/account-creation.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    function createAccount() {
+
+    }
+</script>
+
+<div>
+    <h2>
+        Click here to create your account:
+    </h2>
+
+    <button on:click={createAccount} />
+</div>
