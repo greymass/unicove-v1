92848eecfe4981efe29a3eabdec5636a78cff1ff
dafuga <danielfugere28@gmail.com>
2022-04-26 15:47:16 -0400

    enhancement: added account creation popup

--- snowpack.config.js
+++ snowpack.config.js
@@ -15,6 +15,8 @@ for (const key of forwardEnv) {
     process.env[`SNOWPACK_PUBLIC_${key}`] = process.env[key]
 }
 
+process.env['SNOWPACK_PUBLIC_WHALESPLAINER_URL'] = process.env['WHALESPLAINER_URL'] || 'https://create.anchor.link';
+
 /** @type { import("snowpack").SnowpackUserConfig } */
 module.exports = {
     mount: {
