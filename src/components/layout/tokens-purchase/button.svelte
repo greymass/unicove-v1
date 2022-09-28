acf0f93efdee3568126dfba435f16186f3ae6527
dafuga <danielfugere28@gmail.com>
2022-09-28 17:01:15 -0400

    style: linted

--- src/components/layout/tokens-purchase/button.svelte
+++ src/components/layout/tokens-purchase/button.svelte
@@ -1,17 +1,17 @@
 <script>
-    import Button from "~/components/elements/button.svelte";
-    import { openPopup } from "~/lib/token-purchase";
-    import {activeSession, activeBlockchain} from "~/store";
+    import Button from '~/components/elements/button.svelte'
+    import {openPopup} from '~/lib/token-purchase'
+    import {activeSession, activeBlockchain} from '~/store'
 
-    $: shouldDisplayButton = $activeBlockchain?.id === 'eos';
+    $: shouldDisplayButton = $activeBlockchain?.id === 'eos'
 </script>
 
 <style type="scss">
-  .buy-tokens-button {
-    display: flex;
-    align-items: center;
-    padding: 10px;
-  }
+    .buy-tokens-button {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+    }
 </style>
 
 {#if shouldDisplayButton}
