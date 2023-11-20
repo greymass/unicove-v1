f6430795ad2f5d601f9b0997979cb535649705e8
dafuga <danielfugere28@gmail.com>
2023-11-20 15:33:27 -0800

    chore: added seos, box and usn bridges

--- src/pages/transfer/managers/index.ts
+++ src/pages/transfer/managers/index.ts
@@ -17,6 +17,7 @@ export interface TransferType {
 }
 
 export const transferManagers: {[key: string]: TransferType} = {
+    // EOS Bridges
     'EOS - EOS (EVM)': {
         transferClass: EosEvmBridge,
         tokenName: 'EOS',
@@ -53,6 +54,62 @@ export const transferManagers: {[key: string]: TransferType} = {
         to: 'usdt',
         toLabel: 'USDT',
     },
+    'SEOS - SEOS (EVM)': {
+        transferClass: EosEvmBridge,
+        tokenName: 'SEOS',
+        tokenContract: '0xbfb10f85b889328e4a42507e31a07977ae00eec6',
+        from: 'seos',
+        fromLabel: 'SEOS',
+        to: 'evm',
+        toLabel: 'SEOS (EVM)',
+    },
+    'SEOS (EVM) - SEOS': {
+        transferClass: EvmEosBridge,
+        tokenName: 'SEOS (EVM)',
+        tokenContract: 'eosio.evm',
+        from: 'evm',
+        fromLabel: 'SEOS (EVM)',
+        to: 'seos',
+        toLabel: 'SEOS',
+    },
+    'BOX - BOX (EVM)': {
+        transferClass: EosEvmBridge,
+        tokenName: 'BOX',
+        tokenContract: 'token.box',
+        from: 'box',
+        fromLabel: 'BOX',
+        to: 'evm',
+        toLabel: 'BOX (EVM)',
+    },
+    'BOX (EVM) - BOX': {
+        transferClass: EvmEosBridge,
+        tokenName: 'BOX (EVM)',
+        tokenContract: 'eosio.evm',
+        from: 'evm',
+        fromLabel: 'BOX (EVM)',
+        to: 'box',
+        toLabel: 'BOX',
+    },
+    'USN - USN (EVM)': {
+        transferClass: EosEvmBridge,
+        tokenName: 'USN',
+        tokenContract: 'danchortoken',
+        from: 'usn',
+        fromLabel: 'USN',
+        to: 'evm',
+        toLabel: 'USN (EVM)',
+    },
+    'USN (EVM) - USN': {
+        transferClass: EvmEosBridge,
+        tokenName: 'USN (EVM)',
+        tokenContract: 'eosio.evm',
+        from: 'evm',
+        fromLabel: 'USN (EVM)',
+        to: 'usn',
+        toLabel: 'USN',
+    },
+
+    // Telos Bridges
     'TLOS - TLOS (EVM)': {
         transferClass: TelosEvmBridge,
         tokenName: 'TLOS',
